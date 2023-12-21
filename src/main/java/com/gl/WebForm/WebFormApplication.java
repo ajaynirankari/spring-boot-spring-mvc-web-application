532b202326f6e08bda17369415b1e6a92fe0bd8c
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-12-21 11:25:28 +0530

    added the login form

--- src/main/java/com/gl/WebForm/WebFormApplication.java
+++ src/main/java/com/gl/WebForm/WebFormApplication.java
@@ -20,59 +20,107 @@ public class WebFormApplication {
 @Controller
 class WebController {
 
-	@GetMapping("/greeting")
-	public String greetingForm(Model model) {
-		System.out.println("greetingForm called model = " + model);
-		model.addAttribute("greetingRequestObjectKey", new Greeting());
-		return "greetingPage";
-	}
+    @GetMapping("/")
+    public String loginForm(Model model) {
+        System.out.println("loginForm called model = " + model);
+        model.addAttribute("loginRequestObjectKey", new Login());
+        return "loginFormPage";
+    }
 
-	@PostMapping("/create/greeting")
-	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
-		System.out.println("greetingSubmit called, greeting = " + greeting);
-		model.addAttribute("greetingResponseObjectKey", greeting);
+    @PostMapping("/validate/login")
+    public String validateLoginPageForm(@ModelAttribute Login login, Model model) {
+        System.out.println("validateLoginPageForm called, greeting = " + login);
+        model.addAttribute("loginResponseObjectKey", login);
 
-		return "resultPage";
-	}
+        if ("admin".equals(login.getUserName()) && "admin".equals(login.getPassword())) {
+            System.out.println("loginSuccessFormPage");
+            return "loginSuccessFormPage";
+        } else {
+            System.out.println("loginFailFormPage");
+            return "loginFailFormPage";
+        }
+    }
 
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        System.out.println("greetingForm called model = " + model);
+        model.addAttribute("greetingRequestObjectKey", new Greeting());
+        return "greetingPage";
+    }
+
+    @PostMapping("/create/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        System.out.println("greetingSubmit called, greeting = " + greeting);
+        model.addAttribute("greetingResponseObjectKey", greeting);
+        return "resultPage";
+    }
+}
+
+class Login {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Login{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
 
 class Greeting {
 
-	private long id;
-	private String content;
-	private String type;
+    private long id;
+    private String content;
+    private String type;
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
 
-	@Override
-	public String toString() {
-		return "Greeting{" +
-				"id=" + id +
-				", content='" + content + '\'' +
-				", type='" + type + '\'' +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
